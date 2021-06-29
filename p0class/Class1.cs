9afe97aac589d35cddc87aa0c40f39e6e5b835ca
mstringer2017 <matthewstringer@yahoo.com>
2021-06-29 16:55:14 -0400

    Created Customer Class

---
+++ p0class/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic
+
+namespace p0class
+{
+    public class Customer
+    {
+        public string Name {set; get;}
+
+        public string Address {set; get;}
+
+        public string Email {set; get;}
+
+        public List<string> Orders {get;}
+
+        public void addOrder(string);
+
+    }
+}
