96849a2892da7a161b00b768ae25b9b8960fff99
mstringer2017 <matthewstringer@yahoo.com>
2021-06-30 15:19:01 -0400

    Changed default filenames

---
+++ UI/P0UI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using p0class;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+
+namespace P0UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "./datafiles/datastore.json";
+            string jsonString = File.ReadAllText(filename);
+            List<Customer> customerList = JsonSerializer.Deserialize<List<Customer>>(jsonString);
+
+            bool customerRepeat = true;
+            while (customerRepeat) {
+                Customer customer = new Customer();
+                Console.WriteLine("Name?");
+                customer.Name = Console.ReadLine();
+                Console.WriteLine("Address?");
+                customer.Address = Console.ReadLine();
+                Console.WriteLine("Email?");
+                customer.Email = Console.ReadLine();
+                bool orderRepeat = true;
+                while (orderRepeat) {
+                    Console.WriteLine("Order? (empty line to finish)");
+                    string order = Console.ReadLine();
+                    if (order == "") {
+                        orderRepeat = false;
+                    } else {
+                        customer.Orders.Add(order);
+                    }
+                }
+                customerList.Add(customer);
+                Console.WriteLine("Continue? (y/n)");
+                customerRepeat = Console.ReadLine() == "y";
+            }
+            foreach (Customer customer in customerList) {
+                Console.WriteLine("Name: " + customer.Name);
+                Console.WriteLine("Address: " + customer.Address);
+                Console.WriteLine("Email: " + customer.Email);
+                Console.WriteLine("Orders:");
+                foreach (string item in customer.Orders) {
+                    Console.WriteLine(item);
+                }
+            }
+            jsonString = JsonSerializer.Serialize<List<Customer>>(customerList);
+            Console.WriteLine(jsonString);
+            File.WriteAllText(filename, jsonString);
+        }
+    }
+}
---
+++ p0class/P0Class.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace p0class
+{
+    public class Customer
+    {
+        public string Name {set; get;}
+
+        public string Address {set; get;}
+
+        public string Email {set; get;}
+
+        public List<string> Orders {set; get;}
+
+        public Customer()
+        {
+            Orders = new List<string>();
+        }
+    }
+}
